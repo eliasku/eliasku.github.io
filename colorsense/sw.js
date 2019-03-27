5b80e4575b4aaf1149ea92a70d7b4ef687eccde3
Ilya Kuzmichev <ilyak@playtika.com>
2019-03-27 17:02:55 +0300

    [pwa] fix audit score

--- colorsense/sw.js
+++ colorsense/sw.js
@@ -1,35 +1,35 @@
-var version = "1.0.5-rc.1";
-var cacheName = "colorsense-${version}";
+var cacheName = "colorsense_1.0.5_2";
+
 self.addEventListener('install', function (e) {
-	var timeStamp = Date.now();
-	e.waitUntil(
-		caches.open(cacheName).then(function (cache) {
-			return cache.addAll([
-				'/colorsense/',
-				'/colorsense/index.html',
-				'/colorsense/pwacompat.min.js',
-				'/colorsense/colorsense.js',
-				'/colorsense/colorsense.wasm',
-				'/colorsense/colorsense.data'
-			]);
-		}).then(function () {
-			self.skipWaiting();
-		})
-	);
+    var timeStamp = Date.now();
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll([
+                '/colorsense/',
+                '/colorsense/index.html',
+                '/colorsense/pwacompat.min.js',
+                '/colorsense/colorsense.js',
+                '/colorsense/colorsense.wasm',
+                '/colorsense/colorsense.data'
+            ]);
+        }).then(function () {
+            self.skipWaiting();
+        })
+    );
 });
 
 self.addEventListener('activate', function (event) {
-	event.waitUntil(self.clients.claim());
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', function (event) {
-	event.respondWith(
-		caches.open(cacheName)
-			.then(function (cache) {
-				cache.match(event.request, {ignoreSearch: true});
-			})
-			.then(function (response) {
-				return response || fetch(event.request);
-			})
-	);
+    event.respondWith(
+        caches.open(cacheName)
+            .then(function (cache) {
+                cache.match(event.request, {ignoreSearch: true});
+            })
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+    );
 });
